6bde4b5e7d029c9f15fd5279b0487feb25f7f03e
Teeterok <Teeteroki@gmail.com>
2023-05-29 11:06:36 +0300

    first task

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 "use strict";
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "Введите число");
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = +prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = +prompt("На сколько оцените его?", "");
+const answer = movies[a]
+const personalMovieDB {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+
+}
+personalMovieDB.movies[a] = b
+personalMovieDB.movies[c] = d
